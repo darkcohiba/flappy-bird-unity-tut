e9169612dd10826cc7b64f8ba72d0e5911e29daa
Sam <samuel.waters@gmail.com>
2023-07-16 00:10:02 -0600

    chore: fixed spacing, both sides of pipes spawn into bounds, moving onto adding clouds

--- Assets/CloudSpawnScript.cs
+++ Assets/CloudSpawnScript.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class CloudSpawnScript : MonoBehaviour
 {
-        public GameObject cloud;
+    public GameObject cloud;
+    private float startTime;
+    private float interval = 2f; // Time interval in seconds
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // startTime = Time.time;
+        spawnCloud();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float elapsedTime = Time.time - startTime;
+
+        if (elapsedTime >= interval)
+        {
+            // Perform your actions here
+            Debug.Log("One second has passed!");
+            spawnCloud();
+
+            // Reset the start time
+            startTime = Time.time;
+        }
+    }
+
+    void spawnCloud()
+    {
+        float lowestPoint = -8.1f;
+        float highestPoint = 8.3f;
+        float yCoord = Random.Range(lowestPoint, highestPoint);
+        Instantiate(cloud, new Vector3(transform.position.x, yCoord, 0), transform.rotation);
+
+    
+
     }
 }
--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -18,7 +18,7 @@ public class PipeSpawnScript : MonoBehaviour
     void Start()
     {
         spawnPipe();
-        startTime = Time.time;
+        
 
     }
 
