31b7df89dc8716dbeae5288d9451bd7e7713c6e0
Sam <samuel.waters@gmail.com>
2023-07-16 00:17:15 -0600

    chore: adding speed to clouds

--- Assets/CloudMoveScript.cs
+++ Assets/CloudMoveScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CloudMoveScript : MonoBehaviour
 {
+    public float moveSpeed = 8;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class CloudMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < -35)
+        {
+            Destroy(gameObject);
+        }
     }
+    
 }
